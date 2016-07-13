055760814212c8f04e9bacb75a78aed6770a04c2
fp <ferak.peter@gmail.com>
2016-07-13 21:47:33 +0200

    added the web scrapper example

---
+++ web-scrapper1/project.clj
@@ -0,0 +1,9 @@
+(defproject dojo-spec "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                [org.clojure/data.fressian "0.2.1"]
+                [org.clojure/test.check "0.9.0"]
+                [enlive "1.1.1"]])
---
+++ web-scrapper1/test/dojo_spec/core_test.clj
@@ -0,0 +1,7 @@
+(ns dojo-spec.core-test
+  (:require [clojure.test :refer :all]
+            [dojo-spec.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
